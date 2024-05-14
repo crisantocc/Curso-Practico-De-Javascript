d3a4964d4386184bccc100f21871b2929fac9fcc
crisantocc <crisanto.cervantes.cazares@gmail.com>
2024-05-14 00:17:00 -0500

    Create includes-indexOf-lastIndexOf.js
    // indexOf()
    // lastIndexOf()

---
+++ includes-indexOf-lastIndexOf.js
@@ -0,0 +1,29 @@
+// includes() whit numbers
+
+const numbers = [1,2,3,4,5]
+
+const result1 =numbers.includes(3)
+console.log(result1)
+
+const result2 = numbers.includes(8)
+console.log(result2)
+
+// indexOf()
+
+const fruits = ['apple', 'cherry', 'grape', 'mango']
+
+const index1 = fruits.indexOf('grape')
+console.log(index1)
+
+const index2 = fruits.indexOf('blueberry')
+console.log(index2)
+
+// lastIndexOf()
+
+const numbersAgan = [2,4,6,8,10,6]
+
+const lastIndex1 = numbersAgan.lastIndexOf(6)
+console.log(lastIndex1)
+
+const lastIndex2 = numbersAgan.lastIndexOf(3)
+console.log(lastIndex2)
