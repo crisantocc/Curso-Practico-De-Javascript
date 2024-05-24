c0ecc4990c9e88464746997669156ea7a7050b7f
crisantocc <crisanto.cervantes.cazares@gmail.com>
2024-05-24 15:05:29 -0500

    Update spread-operator.js
    // 2. Combining Arrays
    // 3. Creating Arrays whit Additional Elements
    // 4. Pass elements to fuctions

--- spread-operator.js
+++ spread-operator.js
@@ -5,3 +5,32 @@ const copyOfAnArray = [...originalArray]
 
 console.log(originalArray)
 console.log(copyOfAnArray)
+
+// 2. Combining Arrays
+
+const array1 = [1,2,3]
+const array2 = [4, 5, 6]
+const combinedArray = [...array1, ...array2]
+
+console.log(array1)
+console.log(array2)
+console.log(combinedArray)
+
+// 3. Creating Arrays whit Additional Elements
+
+const baseArray = [1,2,3]
+const arrayWhitAdditionalElements = [...baseArray, 4, 5, 6]
+
+console.log(baseArray)
+console.log(arrayWhitAdditionalElements)
+
+// 4. Pass elements to fuctions
+
+function sum(a, b, c){
+    return a + b + c
+}
+
+const numbers = [1, 2, 3]
+const result = sum(...numbers)
+
+console.log(result)
